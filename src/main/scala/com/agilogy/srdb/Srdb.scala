62414862aabd898911efe5299f5c6614bdc03b07
Jordi Pradel Miquel <jordi.pradel@agilogy.com>
2015-03-04 10:13:38 +0100

    Move functions and types to package object

--- src/main/scala/com/agilogy/srdb/Srdb.scala
+++ src/main/scala/com/agilogy/srdb/Srdb.scala
@@ -2,38 +2,9 @@ package com.agilogy.srdb
 
 import java.sql._
 
-import scala.collection.mutable.ListBuffer
 import scala.util.control.NonFatal
 
 class Srdb private[srdb](exceptionTranslator: ExceptionTranslator) {
-
-  type Reader[T] = ResultSet => T
-  type ArgumentsSetter = PreparedStatement => Unit
-  type Argument = (PreparedStatement, Int) => Unit
-
-  def withExceptionTranslator(et: ExceptionTranslator): Srdb = new Srdb(et)
-
-  trait ExecutableQuery[RT] {
-    
-    private def argumentsSetter(args: Seq[Argument]): ArgumentsSetter = {
-      ps =>
-        args.zipWithIndex.foreach {
-          case (arg, pos) => arg(ps, pos + 1)
-        }
-    }
-
-    def apply(conn: Connection, args: Argument*): RT = apply(conn, argumentsSetter(args))
-
-    def apply(conn: Connection, args: ArgumentsSetter): RT
-  }
-
-  trait ReadableQuery {
-    def raw[T](readResultSet: Reader[T]): ExecutableQuery[T]
-
-    def apply[T](rowReader: Reader[T]): ExecutableQuery[Seq[T]] = raw(asList(rowReader))
-
-    def single[T](rowReader: Reader[T]): ExecutableQuery[Option[T]] = raw(asSingle(rowReader))
-  }
 
   def select(query: String): ReadableQuery = new ReadableQuery {
     override def raw[T](readResultSet: Reader[T]): ExecutableQuery[T] = new ExecutableQuery[T] {
@@ -120,26 +91,6 @@ class Srdb private[srdb](exceptionTranslator: ExceptionTranslator) {
     } finally {
       s.close()
     }
-  }
-
-  private def asList[T](reader: Reader[T]): (ResultSet) => Seq[T] = {
-    rs =>
-      val res = new ListBuffer[T]
-      while (rs.next) {
-        res.append(secure(reader(rs)))
-      }
-      res.toSeq
-  }
-
-  private def asSingle[T](reader: Reader[T]): (ResultSet) => Option[T] = {
-    rs =>
-      if (!rs.next()) {
-        None
-      } else {
-        val res = Some(reader(rs))
-        if (rs.next()) throw new IllegalArgumentException("The query returned more than one row")
-        res
-      }
   }
 
 }
--- src/main/scala/com/agilogy/srdb/package.scala
+++ src/main/scala/com/agilogy/srdb/package.scala
@@ -1,12 +1,62 @@
 package com.agilogy
 
-import java.sql.SQLException
+import java.sql.{Connection, PreparedStatement, ResultSet, SQLException}
 
+import scala.collection.mutable.ListBuffer
 
 package object srdb {
 
   type ExceptionTranslator = SQLException => Exception
 
+  type Reader[T] = ResultSet => T
+  type ArgumentsSetter = PreparedStatement => Unit
+  type Argument = (PreparedStatement, Int) => Unit
+
+  def withExceptionTranslator(et: ExceptionTranslator): Srdb = new Srdb(et)
+
+  trait ExecutableQuery[RT] {
+
+    private def argumentsSetter(args: Seq[Argument]): ArgumentsSetter = {
+      ps =>
+        args.zipWithIndex.foreach {
+          case (arg, pos) => arg(ps, pos + 1)
+        }
+    }
+
+    def apply(conn: Connection, args: Argument*): RT = apply(conn, argumentsSetter(args))
+
+    def apply(conn: Connection, args: ArgumentsSetter): RT
+  }
+
+  trait ReadableQuery {
+    def raw[T](readResultSet: Reader[T]): ExecutableQuery[T]
+
+    def apply[T](rowReader: Reader[T]): ExecutableQuery[Seq[T]] = raw(asList(rowReader))
+
+    def single[T](rowReader: Reader[T]): ExecutableQuery[Option[T]] = raw(asSingle(rowReader))
+  }
+
   val Srdb = new Srdb(identity[SQLException])
+
+
+  private def asList[T](reader: Reader[T]): (ResultSet) => Seq[T] = {
+    rs =>
+      val res = new ListBuffer[T]
+      while (rs.next) {
+        res.append(reader(rs))
+      }
+      res.toSeq
+  }
+
+  private def asSingle[T](reader: Reader[T]): (ResultSet) => Option[T] = {
+    rs =>
+      if (!rs.next()) {
+        None
+      } else {
+        val res = Some(reader(rs))
+        if (rs.next()) throw new IllegalArgumentException("The query returned more than one row")
+        res
+      }
+  }
 
 }
